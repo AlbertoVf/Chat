febd33b63b65beed7d933a1b1a56fd17f94e2890
AlbertoVf <albertoVf97@protonmail.com>
2019-04-18 15:32:05 +0200

    Mensaje
    Visualizacion del mensaje. Actualizacion en tiempo real de usuarios conectados

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,24 +1,54 @@
 $(function() {
     const SOCKET = io(); //conexion en tiempo real
-    const MESSAGEFORM = $("#message-form");
-    const MESSAGE = $("#message");
-    const CHAT = $("#chat");
 
-    const NICKFORM = $("#nickForm");
-    const NICKNAME = $("#nickname");
-    const NICKERROR = $("#nickError");
-    const USERS = $("#usernames");
+    /* VARIABLES DE LA INTERFAZ */
 
+    //Registro de usuarios
+    const NICKFORM = $("#nickForm"); //formulario de usuario
+    const NICKNAME = $("#nickname"); //nombre del usuario
+    const NICKERROR = $("#nickError"); //seccion de errores
+    const USERS = $("#usernames"); //visual de usuarios registrados
+
+    //Envio de mensajes
+    const MESSAGEFORM = $("#message-form"); //formulario de mensaje
+    const MESSAGE = $("#message"); //contenido del mensaje
+    const CHAT = $("#chat"); //seccion para visualizar los mensajes
+
+    /**
+	 * Actualiza la visual de los mensajes mostrando usuario: mensaje
+	 * @param  {object} data Contenido del mensaje
+	 * @return {HTML}      visual del mensaje
+	 */
+    SOCKET.on("new message", function(data) {
+        CHAT.append("<b>" + data.nick + ":</b> " + data.msg + "<br/>");
+    });
+
+    /**
+	 * Actualiza la visual de la lista de usuarios
+	 * @type {HTML}
+	 */
+    SOCKET.on("usernames", data => {
+        let html = "";
+        for (var i = 0; i < data.length; i++) {
+            html += "<p class='user'><i class='fas fa-user'> </i>" + data[i] + "</p>";
+        }
+        USERS.html(html);
+    });
+
+    /**
+	 * Accion al enviar un mensaje. Envia el contenido del mensaje y limpia el campo.
+	 * @type {void}
+	 */
     MESSAGEFORM.submit(e => {
         e.preventDefault();
         SOCKET.emit('send', MESSAGE.val());
         MESSAGE.val("");
     });
 
-    SOCKET.on("new message", function(data) {
-        CHAT.append(data + "<br/>");
-    });
-
+    /**
+	 * Visual para comprobar la existencia de un usuario
+	 * @type {HTML} cambia la vista si el usuario es valido. Si el usuario no es valido muestra un mensaje de error
+	 */
     NICKFORM.submit(e => {
         e.preventDefault();
         SOCKET.emit("new user", NICKNAME.val(), data => {
--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,17 @@
 module.exports = function(IO) {
-    let nicknames = ['usuario', 'usuario2',];
+    let nicknames = []; //lista de usuarios
     /**
-	 * Establecer accion al conectarse
+	 * Actualiza la lista de usuarios
+	 * @return {void} Lista actualizada de los usuarios en linea
+	 */
+    function updateNicknames() {
+        IO
+            .sockets
+            .emit("usernames", nicknames);
+    }
+    /**
+	 * Establece una nueva conexion xon un usuario
+	 * @type {void} Envio de mensajes y lista de los usuarios conectados
 	 */
     IO.on("connection", socket => {
         socket.on("new user", (data, cb) => {
@@ -11,13 +21,32 @@ module.exports = function(IO) {
                 cb(true);
                 socket.nickname = data;
                 nicknames.push(socket.nickname);
+                updateNicknames();
             }
         });
 
+        /**
+		 * Envio de mensajes
+		 * @param  {object} data Contenido del mensaje
+		 * @return {object}      Nuevo mensaje con el contenido y el usuario
+		 */
         socket.on("send", function(data) {
             IO
                 .sockets
-                .emit("new message", data);
+                .emit("new message", {
+                    msg: data,
+                    nick: socket.nickname
+                });
+        });
+        /**
+		 * Accion al desconectarse un usuario
+		 * @type {void} Actualiza la lista de usuarios
+		 */
+        socket.on("disconect", data => {
+            if (!socket.nickname) 
+                return;
+            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            updateNicknames();
         });
 
     });
