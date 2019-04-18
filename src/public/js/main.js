2dbe70aafab03c9bfba48456f8dfb20fd1f9a63a
AlbertoVf <albertoVf97@protonmail.com>
2019-04-18 12:50:56 +0200

    Chat
    Envio de mensajes y mostrarlos en pantalla

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,16 @@
-console.log( 'chat aqui' );
+$(function() {
+    const SOCKET = io(); //conexion en tiempo real
+    const MESSAGEFORM = $("#message-form");
+    const MESSAGE = $("#message");
+    const CHAT = $("#chat");
+
+    MESSAGEFORM.submit(e => {
+        e.preventDefault();
+        SOCKET.emit('send', MESSAGE.val());
+        MESSAGE.val("");
+    });
+
+    SOCKET.on("new message", function(data) {
+        CHAT.append(data + "<br/>");
+    });
+});
--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,13 @@
-module.exports = function ( IO ) {
+module.exports = function(IO) {
     /**
 	 * Establecer accion al conectarse
 	 */
-    IO.on( "connection", socket => {
-        console.log( "nuevo usuario conectado" );
-    } );
+    IO.on("connection", socket => {
+        console.log("nuevo usuario conectado");
+        socket.on("send", function(data) {
+            IO
+                .sockets
+                .emit("new message", data);
+        });
+    });
 }
