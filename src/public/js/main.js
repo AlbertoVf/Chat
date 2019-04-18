b2a0d96ba23cffb7a7723c5015c9ea3668af006c
AlbertoVf <albertoVf97@protonmail.com>
2019-04-18 14:27:19 +0200

    Login
    Comprobacion de registro de usuario

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,6 +4,11 @@ $(function() {
     const MESSAGE = $("#message");
     const CHAT = $("#chat");
 
+    const NICKFORM = $("#nickForm");
+    const NICKNAME = $("#nickname");
+    const NICKERROR = $("#nickError");
+    const USERS = $("#usernames");
+
     MESSAGEFORM.submit(e => {
         e.preventDefault();
         SOCKET.emit('send', MESSAGE.val());
@@ -12,5 +17,18 @@ $(function() {
 
     SOCKET.on("new message", function(data) {
         CHAT.append(data + "<br/>");
+    });
+
+    NICKFORM.submit(e => {
+        e.preventDefault();
+        SOCKET.emit("new user", NICKNAME.val(), data => {
+            if (data) {
+                $("#nickWrap").hide();
+                $("#contentWrap").show();
+            } else {
+                NICKERROR.html('<div class="alert alert-danger">El usuario ya existe.</div>')
+            }
+            NICKNAME.val("");
+        });
     });
 });
--- src/socket.js
+++ src/socket.js
@@ -1,13 +1,24 @@
 module.exports = function(IO) {
+    let nicknames = ['usuario', 'usuario2',];
     /**
 	 * Establecer accion al conectarse
 	 */
     IO.on("connection", socket => {
-        console.log("nuevo usuario conectado");
+        socket.on("new user", (data, cb) => {
+            if (nicknames.indexOf(data) != -1) {
+                cb(false);
+            } else {
+                cb(true);
+                socket.nickname = data;
+                nicknames.push(socket.nickname);
+            }
+        });
+
         socket.on("send", function(data) {
             IO
                 .sockets
                 .emit("new message", data);
         });
+
     });
 }
